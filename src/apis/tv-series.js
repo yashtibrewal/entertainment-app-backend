e112b66e5266dd0dbd3fb119881bdf881613f699
Yash Tibrewal <yashkush.tibrewal@gmail.com>
2024-11-26 13:18:11 +0530

    added get tv detail

--- src/apis/tv-series.js
+++ src/apis/tv-series.js
@@ -26,4 +26,26 @@ tvSeriesRouter.get("/trending/list", passport.authenticate('jwt', { session: fal
 
 });
 
+tvSeriesRouter.get("/:id", passport.authenticate('jwt', { session: false }), async (req, res) => {
+
+  const url = `${process.env.TMDB_URL}/tv/${req.params.id}?language=en-US`;
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${process.env.TMDB_API_TOKEN}`
+    }
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+
+});
+
 module.exports = tvSeriesRouter;
